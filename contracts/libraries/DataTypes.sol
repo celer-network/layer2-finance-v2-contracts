6f230f4ebce4ea3cf0c52893f03f7051f824cb98
Raja <raja@celer.network>
2021-04-29 15:13:01 -0700

    Data types for the new L2F flow. (#2)

---
+++ contracts/libraries/DataTypes.sol
@@ -0,0 +1,184 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.6.0 <0.9.0;
+
+library DataTypes {
+    struct Block {
+        bytes32 rootHash;
+        bytes32 intentHash; // hash of L2-to-L1 commitment sync transitions
+        uint32 intentExecCount; // count of intents executed so far (MAX_UINT32 == all done)
+        uint32 blockSize; // number of transitions in the block
+        uint64 blockTime; // blockNum when this rollup block is committed
+    }
+
+    struct InitTransition {
+        uint8 transitionType;
+        bytes32 stateRoot;
+    }
+
+    struct DepositTransition {
+        uint8 transitionType;
+        bytes32 stateRoot;
+        address account; // must provide L1 address for "pending deposit" handling
+        uint32 accountId; // needed for transition evaluation in case of dispute
+        uint32 assetId;
+        uint256 amount;
+    }
+
+    struct WithdrawTransition {
+        uint8 transitionType;
+        bytes32 stateRoot;
+        address account; // must provide L1 target address for "pending withdraw" handling
+        uint32 accountId;
+        uint32 assetId;
+        uint256 amount;
+        uint256 fee; // in units of asset; signed by the user
+        uint64 timestamp; // Unix epoch (msec, UTC)
+        bytes signature;
+    }
+
+    struct BuyTransition {
+        uint8 transitionType;
+        bytes32 stateRoot;
+        uint32 accountId;
+        uint32 strategyId;
+        uint256 amount;
+        uint256 maxSharePrice;
+        uint256 fee; // in units of asset; signed by the user
+        uint64 timestamp; // Unix epoch (msec, UTC)
+        bytes signature;
+    }
+
+    struct SellTransition {
+        uint8 transitionType;
+        bytes32 stateRoot;
+        uint32 accountId;
+        uint32 strategyId;
+        uint256 shares;
+        uint256 minSharePrice;
+        uint256 fee; // in units of asset; signed by the user
+        uint64 timestamp; // Unix epoch (msec, UTC)
+        bytes signature;
+    }
+
+    struct TransferAssetTransition {
+        uint8 transitionType;
+        bytes32 stateRoot;
+        uint32 fromAccountId;
+        uint32 toAccountId;
+        uint32 assetId;
+        uint256 amount;
+        uint256 fee; // in units of asset; signed by the user
+        uint64 timestamp; // Unix epoch (msec, UTC)
+        bytes signature;
+    }
+
+    struct TransferShareTransition {
+        uint8 transitionType;
+        bytes32 stateRoot;
+        uint32 fromAccountId;
+        uint32 toAccountId;
+        uint32 strategyId;
+        uint256 shares;
+        uint256 fee; // in units of asset; signed by the user
+        uint64 timestamp; // Unix epoch (msec, UTC)
+        bytes signature;
+    }
+
+    struct AggregateOrdersTransition {
+        uint8 transitionType;
+        bytes32 stateRoot;
+        uint32 strategyId;
+        uint64 aggregateId;
+        uint256 buyAmount;
+        uint256 sellShares;
+        uint256 minSharesFromBuy;
+        uint256 minAmountFromSell;
+    }
+
+    struct ExecutionResultTransition {
+        uint8 transitionType;
+        bytes32 stateRoot;
+        uint32 strategyId;
+        uint64 aggregateId;
+        bool success;
+        uint256 sharesFromBuy;
+        uint256 amountFromSell;
+    }
+
+    struct SettlementTransition {
+        uint8 transitionType;
+        bytes32 stateRoot;
+        uint32 strategyId;
+        uint64 aggregateId;
+        uint32 accountId;
+    }
+
+    // Pending account actions (buy/sell) per account, strategy, aggregateId.
+    // The array of PendingAccountInfo structs is sorted by ascending aggregateId, and holes are ok.
+    struct PendingAccountInfo {
+        int64 aggregateId;
+        uint256 buyAmount;
+        uint256 buyFees;
+        uint256 sellShares;
+        uint256 sellFees;
+    }
+
+    struct AccountInfo {
+        address account;
+        uint32 accountId; // mapping only on L2 must be part of stateRoot
+        uint256[] idleAssets; // indexed by assetId
+        uint256[] shares; // indexed by strategyId
+        PendingAccountInfo[][] pending; // indexed by [strategyId][i], i.e. array of pending records per strategy
+        uint64 timestamp; // Unix epoch (msec, UTC)
+    }
+
+    // Pending strategy actions per strategy, aggregateId.
+    // The array of PendingStrategyInfo structs is sorted by ascending aggregateId, and holes are ok.
+    struct PendingStrategyInfo {
+        int64 aggregateId;
+        uint256 maxSharePriceForBuy;
+        uint256 minSharePriceForSell;
+        uint256 buyAmount;
+        uint256 sellShares;
+        uint256 sharesFromBuy;
+        uint256 amountFromSell;
+        uint256 unsettledBuyAmount;
+        uint256 unsettledSellShares;
+        bool executionSucceed;
+    }
+
+    struct StrategyInfo {
+        uint32 assetId;
+        uint256 assetBalance;
+        uint256 shareSupply;
+        uint64 nextAggregateId;
+        uint64 lastExecAggregateId;
+        PendingStrategyInfo[] pending; // array of pending records
+    }
+
+    struct TransitionProof {
+        bytes transition;
+        uint256 blockId;
+        uint32 index;
+        bytes32[] siblings;
+    }
+
+    // Even when the disputed transition only affects an account without a strategy or only
+    // affects a strategy without an account, both AccountProof and StrategyProof must be sent
+    // to at least give the root hashes of the two separate Merkle trees (account and strategy).
+    // Each transition stateRoot = hash(accountStateRoot, strategyStateRoot).
+    struct AccountProof {
+        bytes32 stateRoot; // for the account Merkle tree
+        AccountInfo value;
+        uint32 index;
+        bytes32[] siblings;
+    }
+
+    struct StrategyProof {
+        bytes32 stateRoot; // for the strategy Merkle tree
+        StrategyInfo value;
+        uint32 index;
+        bytes32[] siblings;
+    }
+}
---
+++ contracts/libraries/Transitions.sol
@@ -0,0 +1,271 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.6.0 <0.9.0;
+pragma experimental ABIEncoderV2;
+
+import "../libraries/DataTypes.sol";
+
+library Transitions {
+    // Transition Types
+    uint8 public constant TRANSITION_TYPE_INVALID = 0;
+    uint8 public constant TRANSITION_TYPE_INIT = 1;
+    uint8 public constant TRANSITION_TYPE_DEPOSIT = 2;
+    uint8 public constant TRANSITION_TYPE_WITHDRAW = 3;
+    uint8 public constant TRANSITION_TYPE_BUY = 4;
+    uint8 public constant TRANSITION_TYPE_SELL = 5;
+    uint8 public constant TRANSITION_TYPE_XFER_ASSET = 6;
+    uint8 public constant TRANSITION_TYPE_XFER_SHARE = 7;
+    uint8 public constant TRANSITION_TYPE_SETTLE = 8;
+    uint8 public constant TRANSITION_TYPE_SYNC_COMMITMENT = 9;
+    uint8 public constant TRANSITION_TYPE_EXEC_RESULT = 10;
+    uint8 public constant TRANSITION_TYPE_UPDATE_PRICES = 11;
+
+    function extractTransitionType(bytes memory _bytes) internal pure returns (uint8) {
+        uint8 transitionType;
+        assembly {
+            transitionType := mload(add(_bytes, 0x20))
+        }
+        return transitionType;
+    }
+
+    function decodeInitTransition(bytes memory _rawBytes) internal pure returns (DataTypes.InitTransition memory) {
+        (uint8 transitionType, bytes32 stateRoot) = abi.decode((_rawBytes), (uint8, bytes32));
+        DataTypes.InitTransition memory transition = DataTypes.InitTransition(transitionType, stateRoot);
+        return transition;
+    }
+
+    function decodeDepositTransition(bytes memory _rawBytes)
+        internal
+        pure
+        returns (DataTypes.DepositTransition memory)
+    {
+        (uint8 transitionType, bytes32 stateRoot, address account, uint32 accountId, uint32 assetId, uint256 amount) =
+            abi.decode((_rawBytes), (uint8, bytes32, address, uint32, uint32, uint256));
+        DataTypes.DepositTransition memory transition =
+            DataTypes.DepositTransition(transitionType, stateRoot, account, accountId, assetId, amount);
+        return transition;
+    }
+
+    function decodeWithdrawTransition(bytes memory _rawBytes)
+        internal
+        pure
+        returns (DataTypes.WithdrawTransition memory)
+    {
+        (
+            uint8 transitionType,
+            bytes32 stateRoot,
+            address account,
+            uint32 accountId,
+            uint32 assetId,
+            uint256 amount,
+            uint256 fee,
+            uint64 timestamp,
+            bytes memory signature
+        ) = abi.decode((_rawBytes), (uint8, bytes32, address, uint32, uint32, uint256, uint256, uint64, bytes));
+        DataTypes.WithdrawTransition memory transition =
+            DataTypes.WithdrawTransition(
+                transitionType,
+                stateRoot,
+                account,
+                accountId,
+                assetId,
+                amount,
+                fee,
+                timestamp,
+                signature
+            );
+        return transition;
+    }
+
+    function decodeBuyTransition(bytes memory _rawBytes) internal pure returns (DataTypes.BuyTransition memory) {
+        (
+            uint8 transitionType,
+            bytes32 stateRoot,
+            uint32 accountId,
+            uint32 strategyId,
+            uint256 amount,
+            uint256 minShares,
+            uint256 fee,
+            uint64 timestamp,
+            bytes memory signature
+        ) = abi.decode((_rawBytes), (uint8, bytes32, uint32, uint32, uint256, uint256, uint256, uint64, bytes));
+        DataTypes.BuyTransition memory transition =
+            DataTypes.BuyTransition(
+                transitionType,
+                stateRoot,
+                accountId,
+                strategyId,
+                amount,
+                minShares,
+                fee,
+                timestamp,
+                signature
+            );
+        return transition;
+    }
+
+    function decodeSellTransition(bytes memory _rawBytes) internal pure returns (DataTypes.SellTransition memory) {
+        (
+            uint8 transitionType,
+            bytes32 stateRoot,
+            uint32 accountId,
+            uint32 strategyId,
+            uint256 shares,
+            uint256 minAmount,
+            uint256 fee,
+            uint64 timestamp,
+            bytes memory signature
+        ) = abi.decode((_rawBytes), (uint8, bytes32, uint32, uint32, uint256, uint256, uint256, uint64, bytes));
+        DataTypes.SellTransition memory transition =
+            DataTypes.SellTransition(
+                transitionType,
+                stateRoot,
+                accountId,
+                strategyId,
+                shares,
+                minAmount,
+                fee,
+                timestamp,
+                signature
+            );
+        return transition;
+    }
+
+    function decodeTransferAssetTransition(bytes memory _rawBytes)
+        internal
+        pure
+        returns (DataTypes.TransferAssetTransition memory)
+    {
+        (
+            uint8 transitionType,
+            bytes32 stateRoot,
+            uint32 fromAccountId,
+            uint32 toAccountId,
+            uint32 assetId,
+            uint256 amount,
+            uint256 fee,
+            uint64 timestamp,
+            bytes memory signature
+        ) = abi.decode((_rawBytes), (uint8, bytes32, uint32, uint32, uint32, uint256, uint256, uint64, bytes));
+        DataTypes.TransferAssetTransition memory transition =
+            DataTypes.TransferAssetTransition(
+                transitionType,
+                stateRoot,
+                fromAccountId,
+                toAccountId,
+                assetId,
+                amount,
+                fee,
+                timestamp,
+                signature
+            );
+        return transition;
+    }
+
+    function decodeTransferShareTransition(bytes memory _rawBytes)
+        internal
+        pure
+        returns (DataTypes.TransferShareTransition memory)
+    {
+        (
+            uint8 transitionType,
+            bytes32 stateRoot,
+            uint32 fromAccountId,
+            uint32 toAccountId,
+            uint32 strategyId,
+            uint256 shares,
+            uint256 fee,
+            uint64 timestamp,
+            bytes memory signature
+        ) = abi.decode((_rawBytes), (uint8, bytes32, uint32, uint32, uint32, uint256, uint256, uint64, bytes));
+        DataTypes.TransferShareTransition memory transition =
+            DataTypes.TransferShareTransition(
+                transitionType,
+                stateRoot,
+                fromAccountId,
+                toAccountId,
+                strategyId,
+                shares,
+                fee,
+                timestamp,
+                signature
+            );
+        return transition;
+    }
+
+    function decodeSettlementTransition(bytes memory _rawBytes)
+        internal
+        pure
+        returns (DataTypes.SettlementTransition memory)
+    {
+        (uint8 transitionType, bytes32 stateRoot, int64 rubId, uint32 accountId, uint32 strategyId) =
+            abi.decode((_rawBytes), (uint8, bytes32, int64, uint32, uint32));
+        DataTypes.SettlementTransition memory transition =
+            DataTypes.SettlementTransition(transitionType, stateRoot, rubId, accountId, strategyId);
+        return transition;
+    }
+
+    function decodeCommitmentSyncTransition(bytes memory _rawBytes)
+        internal
+        pure
+        returns (DataTypes.CommitmentSyncTransition memory)
+    {
+        (
+            uint8 transitionType,
+            bytes32 stateRoot,
+            uint32 strategyId,
+            uint256 buyAmount,
+            uint256 sellShares,
+            uint256 minSharesFromBuy,
+            uint256 minAmountFromSell
+        ) = abi.decode((_rawBytes), (uint8, bytes32, uint32, uint256, uint256, uint256, uint256));
+        DataTypes.CommitmentSyncTransition memory transition =
+            DataTypes.CommitmentSyncTransition(
+                transitionType,
+                stateRoot,
+                strategyId,
+                buyAmount,
+                sellShares,
+                minSharesFromBuy,
+                minAmountFromSell
+            );
+        return transition;
+    }
+
+    function decodeExecutionResultTransition(bytes memory _rawBytes)
+        internal
+        pure
+        returns (DataTypes.ExecutionResultTransition memory)
+    {
+        (
+            uint8 transitionType,
+            bytes32 stateRoot,
+            int64 rubId,
+            uint32 strategyId,
+            uint256 sharesFromBuy,
+            uint256 amountFromSell
+        ) = abi.decode((_rawBytes), (uint8, bytes32, int64, uint32, uint256, uint256));
+        DataTypes.ExecutionResultTransition memory transition =
+            DataTypes.ExecutionResultTransition(
+                transitionType,
+                stateRoot,
+                rubId,
+                strategyId,
+                sharesFromBuy,
+                amountFromSell
+            );
+        return transition;
+    }
+
+    function decodeUpdatePricesTransition(bytes memory _rawBytes)
+        internal
+        pure
+        returns (DataTypes.UpdatePricesTransition memory)
+    {
+        (uint8 transitionType, bytes32 stateRoot, uint32 strategyId, uint256 maxBuyPrice, uint256 minSellPrice) =
+            abi.decode((_rawBytes), (uint8, bytes32, uint32, uint256, uint256));
+        DataTypes.UpdatePricesTransition memory transition =
+            DataTypes.UpdatePricesTransition(transitionType, stateRoot, strategyId, maxBuyPrice, minSellPrice);
+        return transition;
+    }
+}
