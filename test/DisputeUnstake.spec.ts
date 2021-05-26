d1e1cbf6fb08bbba8daeccb7fb1f65cbbdad6197
Michael Zhou <zhoumotongxue008@gmail.com>
2021-05-27 05:23:38 +0800

    Add more test cases for stake / unstake (#49)

--- test/DisputeUnstake.spec.ts
+++ test/DisputeUnstake.spec.ts
@@ -10,6 +10,13 @@ describe('DisputeUnstake', function () {
     const { rollupChain, celr, dai } = await deployContracts(admin);
     await rollupChain.setBlockChallengePeriod(10);
 
+    const users = await getUsers(admin, [dai], 2);
+    const depositAmount = parseEther('100');
+    await dai.approve(rollupChain.address, depositAmount);
+    await rollupChain.depositReward(dai.address, depositAmount);
+    await dai.connect(users[0]).approve(rollupChain.address, depositAmount);
+    await rollupChain.connect(users[0]).deposit(dai.address, depositAmount);
+
     return {
       admin,
       rollupChain,
@@ -19,14 +26,7 @@ describe('DisputeUnstake', function () {
   }
 
   it('should fail to dispute valid unstake', async function () {
-    const { admin, rollupChain, dai } = await loadFixture(fixture);
-    const users = await getUsers(admin, [dai], 2);
-    const depositAmount = parseEther('100');
-    await dai.approve(rollupChain.address, depositAmount);
-    await rollupChain.depositReward(dai.address, depositAmount);
-    await dai.connect(users[0]).approve(rollupChain.address, depositAmount);
-    await rollupChain.connect(users[0]).deposit(dai.address, depositAmount);
-
+    const { admin, rollupChain } = await loadFixture(fixture);
     const { tns, disputeData } = await parseInput('test/input/data/dispute-unstake-valid.txt');
 
     await rollupChain.commitBlock(0, tns[0]);
@@ -39,5 +39,41 @@ describe('DisputeUnstake', function () {
         data: disputeData
       })
     ).to.be.revertedWith('Failed to dispute');
+  });
+
+  it('should dispute unstake with invalid root', async function () {
+    const { admin, rollupChain } = await loadFixture(fixture);
+    const { tns, disputeData } = await parseInput('test/input/data/dispute-unstake-root.txt');
+
+    await rollupChain.commitBlock(0, tns[0]);
+    await advanceBlockNumberTo(100 - 1);
+    await rollupChain.executeBlock(0, [tns[0][4]], 1);
+    await rollupChain.commitBlock(1, tns[1]);
+    await expect(
+      admin.sendTransaction({
+        to: rollupChain.address,
+        data: disputeData
+      })
+    )
+      .to.emit(rollupChain, 'RollupBlockReverted')
+      .withArgs(1, 'invalid post-state root');
+  });
+
+  it('should dispute unstake with invalid shares', async function () {
+    const { admin, rollupChain } = await loadFixture(fixture);
+    const { tns, disputeData } = await parseInput('test/input/data/dispute-unstake-shares.txt');
+
+    await rollupChain.commitBlock(0, tns[0]);
+    await advanceBlockNumberTo(150 - 1);
+    await rollupChain.executeBlock(0, [tns[0][4]], 1);
+    await rollupChain.commitBlock(1, tns[1]);
+    await expect(
+      admin.sendTransaction({
+        to: rollupChain.address,
+        data: disputeData
+      })
+    )
+      .to.emit(rollupChain, 'RollupBlockReverted')
+      .withArgs(1, 'failed to evaluate');
   });
 });
