0d64ae4f31e72abfc536637394453c6f12862926
Michael Zhou <zhoumotongxue008@gmail.com>
2021-05-26 21:02:51 +0800

    Disable typechain in test_all to speed it up

--- scripts/test_all.sh
+++ scripts/test_all.sh
@@ -1,9 +1,17 @@
 # Run each test with a new network, so that block numbers won't be affected by each other
+# Must be run from the project root directory
 
 set -e
 
-files=`ls test/*.spec.ts`
-for eachfile in $files
-do
+# Hack to disable typechain generation
+HARDHAT_CONFIG_FILE="./hardhat.config.ts"
+sed -e "/import '@typechain\/hardhat';/d;/typechain: {/,/}/d" "$HARDHAT_CONFIG_FILE" >$HARDHAT_CONFIG_FILE.new
+mv "$HARDHAT_CONFIG_FILE" "$HARDHAT_CONFIG_FILE".bak
+mv "$HARDHAT_CONFIG_FILE".new "$HARDHAT_CONFIG_FILE"
+
+files=$(ls test/*.spec.ts)
+for eachfile in $files; do
    hardhat test $eachfile
 done
+
+mv "$HARDHAT_CONFIG_FILE".bak "$HARDHAT_CONFIG_FILE"
