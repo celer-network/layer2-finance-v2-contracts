28b50ade18447ddd5a383b777d133bbc04b146d6
Xiaozhou Li <xli@celer.network>
2021-06-03 22:20:55 -0700

    test execute orders

--- test/BuySell.spec.ts
+++ test/BuySell.spec.ts
@@ -8,13 +8,13 @@ import { advanceBlockNumberTo, deployContracts, getUsers, loadFixture, parseInpu
 
 describe('BuySell', function () {
   async function fixture([admin]: Wallet[]) {
-    const { rollupChain, celr, dai } = await deployContracts(admin);
+    const { rollupChain, dai, weth } = await deployContracts(admin);
 
     return {
       admin,
       rollupChain,
-      celr,
-      dai
+      dai,
+      weth
     };
   }
 
@@ -62,5 +62,86 @@ describe('BuySell', function () {
 
     [, , status] = await rollupChain.pendingExecResults(1, 1);
     expect(status).to.equal(1);
+  });
+
+  it('should execute orders one-by-one correctly', async function () {
+    const { admin, rollupChain, dai, weth } = await loadFixture(fixture);
+    const users = await getUsers(admin, [dai], 3);
+    await dai.connect(users[0]).approve(rollupChain.address, parseEther('100'));
+    await dai.connect(users[1]).approve(rollupChain.address, parseEther('100'));
+    await rollupChain.connect(users[0]).deposit(dai.address, parseEther('100'));
+    await rollupChain.connect(users[1]).deposit(dai.address, parseEther('100'));
+    await rollupChain.connect(users[2]).depositETH(weth.address, parseEther('1'), { value: parseEther('1') });
+
+    const { tns } = await parseInput('test/input/data/execute-orders.txt');
+
+    await rollupChain.commitBlock(0, tns[0]);
+    await rollupChain.commitBlock(1, tns[1]);
+
+    await advanceBlockNumberTo(100 - 1);
+    const intents = [tns[0][6], tns[0][7], tns[0][8]];
+    await expect(rollupChain.executeBlock(0, intents, 1))
+      .to.emit(rollupChain, 'AggregationExecuted')
+      .withArgs(1, 0, true, parseEther('3'), 0, 100)
+      .to.emit(rollupChain, 'RollupBlockExecuted')
+      .withArgs(0, 1);
+
+    await expect(rollupChain.executeBlock(0, intents, 1))
+      .to.emit(rollupChain, 'AggregationExecuted')
+      .withArgs(2, 0, true, parseEther('2'), 0, 101)
+      .to.emit(rollupChain, 'RollupBlockExecuted')
+      .withArgs(0, 2);
+
+    await expect(rollupChain.executeBlock(1, [tns[1][1]], 1)).to.be.revertedWith('invalid block ID');
+
+    await expect(rollupChain.executeBlock(0, intents, 1))
+      .to.emit(rollupChain, 'AggregationExecuted')
+      .withArgs(3, 0, true, parseEther('1'), 0, 103)
+      .to.emit(rollupChain, 'RollupBlockExecuted')
+      .withArgs(0, 3);
+
+    await expect(rollupChain.executeBlock(1, [tns[1][1]], 1))
+      .to.emit(rollupChain, 'AggregationExecuted')
+      .withArgs(1, 1, true, parseEther('5'), 0, 104)
+      .to.emit(rollupChain, 'RollupBlockExecuted')
+      .withArgs(1, 1);
+  });
+
+  it('should execute orders in batch correctly', async function () {
+    const { admin, rollupChain, dai, weth } = await loadFixture(fixture);
+    const users = await getUsers(admin, [dai], 3);
+    await dai.connect(users[0]).approve(rollupChain.address, parseEther('100'));
+    await dai.connect(users[1]).approve(rollupChain.address, parseEther('100'));
+    await rollupChain.connect(users[0]).deposit(dai.address, parseEther('100'));
+    await rollupChain.connect(users[1]).deposit(dai.address, parseEther('100'));
+    await rollupChain.connect(users[2]).depositETH(weth.address, parseEther('1'), { value: parseEther('1') });
+
+    const { tns } = await parseInput('test/input/data/execute-orders.txt');
+
+    await rollupChain.commitBlock(0, tns[0]);
+
+    await advanceBlockNumberTo(150 - 1);
+    const intents = [tns[0][6], tns[0][7], tns[0][8]];
+    await expect(rollupChain.executeBlock(0, intents, 2))
+      .to.emit(rollupChain, 'AggregationExecuted')
+      .withArgs(1, 0, true, parseEther('3'), 0, 150)
+      .to.emit(rollupChain, 'AggregationExecuted')
+      .withArgs(2, 0, true, parseEther('2'), 0, 150)
+      .to.emit(rollupChain, 'RollupBlockExecuted')
+      .withArgs(0, 2);
+
+    let [, , intentExecCount, , ,] = await rollupChain.blocks(0);
+    expect(intentExecCount).to.equal(2);
+
+    await expect(rollupChain.executeBlock(0, intents, 2)).to.be.revertedWith('invalid data length');
+
+    await expect(rollupChain.executeBlock(0, intents, 1))
+      .to.emit(rollupChain, 'AggregationExecuted')
+      .withArgs(3, 0, true, parseEther('1'), 0, 152)
+      .to.emit(rollupChain, 'RollupBlockExecuted')
+      .withArgs(0, 3);
+
+    [, , intentExecCount, , ,] = await rollupChain.blocks(0);
+    expect(intentExecCount).to.equal(2 ** 32 - 1);
   });
 });
