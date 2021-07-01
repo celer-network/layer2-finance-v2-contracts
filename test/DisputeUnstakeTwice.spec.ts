ab83fb5fc03def9c115f0fe0de799f2254f3d98b
Xiaozhou Li <xli@celer.network>
2021-06-30 18:58:02 -0700

    dispute unstake

--- test/DisputeUnstakeTwice.spec.ts
+++ test/DisputeUnstakeTwice.spec.ts
@@ -34,10 +34,12 @@ describe('DisputeUnstakeTwice', function () {
 
     await rollupChain.commitBlock(0, tns[0]);
     await advanceBlockNumberTo(50 - 1);
+    await rollupChain.updateEpoch();
     await rollupChain.executeBlock(0, [tns[0][4]], 1);
     await rollupChain.commitBlock(1, tns[1]);
     await advanceBlockNumberTo(157 - 1);
-    await rollupChain.executeBlock(1, [tns[1][4]], 1);
+    await rollupChain.updateEpoch();
+    await rollupChain.executeBlock(1, [tns[1][5]], 1);
     await rollupChain.commitBlock(2, tns[2]);
     await expect(
       admin.sendTransaction({
