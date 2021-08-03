23bb2ee24f957f51e6333682eeb2bef531b47afc
何智华 <hezhihua@MacBook-Pro.lan>
2021-08-03 17:56:44 +0800

    Strategy Sushiswap

---
+++ contracts/strategies/sushiswap/StrategySushiswap.sol
@@ -0,0 +1,220 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.6;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
+import "./SushiswapLibrary.sol";
+import "../AbstractStrategy.sol";
+import "../interfaces/sushi/IMasterChef.sol";
+import "../interfaces/uniswap/IUniswapV2.sol";
+import "../interfaces/uniswap/IUniswapV2Pair.sol";
+import "../../interfaces/IWETH.sol";
+
+/**
+ * Deposit one token, exchange half of them to another token using sushiswap, and then deposit the pair
+ * into sushiswap for liquidity farming. 
+ */
+contract StrategySushiswap is AbstractStrategy {
+    using SafeERC20 for IERC20;
+    using Address for address;
+
+    // The address of token to be paired
+    address public pairToken;
+    address public sushiswap;
+    address public masterChef;
+    address public sushi;
+    uint256 public maxSlippage;
+    uint256 public maxOneDeposit;
+    // The id of the pool to farm Sushi
+    int256 public poolId;
+    uint256 public lpAmtInPool;
+
+    constructor(
+        address _controller,
+        address _supplyToken,
+        address _pairToken,
+        address _sushiswap,
+        address _masterChef,
+        address _sushi,
+        uint256 _maxSlippage,
+        uint256 _maxOneDeposit,
+        int256 _poolId
+    ) AbstractStrategy(_controller, _supplyToken) {
+        pairToken = _pairToken;
+        sushiswap = _sushiswap;
+        masterChef = _masterChef;
+        sushi = _sushi;
+        maxSlippage = _maxSlippage;
+        maxOneDeposit = _maxOneDeposit;
+        poolId = _poolId;
+    }
+
+    /**
+     * @dev Require that the caller must be an EOA account to avoid flash loans.
+     */
+    modifier onlyEOA() {
+        require(msg.sender == tx.origin, "Not EOA");
+        _;
+    }
+
+    function getAssetAmount() internal view override returns (uint256) {
+        address _supplyToken = supplyToken;
+        address _pairToken = pairToken;
+        IUniswapV2Pair pair = IUniswapV2Pair(SushiswapLibrary.pairFor(IUniswapV2(sushiswap).factory(), _supplyToken, _pairToken));
+        uint256 myLiquidity = pair.balanceOf(address(this)) + lpAmtInPool;
+        if (myLiquidity == 0) {
+            return IERC20(supplyToken).balanceOf(address(this)); // should include the assets not yet adjusted
+        }
+        
+        uint256 totalSupply = pair.totalSupply();
+        (address token0,) = SushiswapLibrary.sortTokens(_supplyToken, _pairToken);
+        (uint reserve0, uint reserve1,) = pair.getReserves();
+        if (_supplyToken != token0) {
+            (reserve0, reserve1) = (reserve1, reserve0);
+        }
+        uint myReserve0 = myLiquidity * reserve0 / totalSupply;
+        uint myReserve1 = myLiquidity * reserve1 / totalSupply;
+        uint myReserve1Out = SushiswapLibrary.getAmountOut(myReserve1, reserve1, reserve0);
+        
+        return myReserve0 + myReserve1Out + IERC20(supplyToken).balanceOf(address(this)); // should include the assets not yet adjusted    
+    }
+
+    function buy(uint256 _buyAmount) internal override returns (uint256) {
+        // Pull supplying token(e.g. USDC, USDT) from Controller
+        IERC20(supplyToken).safeTransferFrom(msg.sender, address(this), _buyAmount);
+
+        // do virtual buy here, actual buy is done in adjust method        
+        return _buyAmount;
+    }
+
+    function sell(uint256 _sellAmount) internal override returns (uint256) {
+        address _supplyToken = supplyToken;
+        uint256 balanceBeforeSell = IERC20(_supplyToken).balanceOf(address(this));
+        uint256 amtFromUniswap;
+
+        if (balanceBeforeSell < _sellAmount) {
+            amtFromUniswap = _sellAmount - balanceBeforeSell;
+        } 
+
+        if (amtFromUniswap > 0) {
+            address _pairToken = pairToken;
+            address _sushiswap = sushiswap;
+            
+            IUniswapV2Pair pair = IUniswapV2Pair(SushiswapLibrary.pairFor(IUniswapV2(_sushiswap).factory(), _supplyToken, _pairToken));
+            uint256 lpInMyAddress = pair.balanceOf(address(this));
+            uint256 toSellLiquidity = (lpInMyAddress + lpAmtInPool) * amtFromUniswap / (getAssetAmount() - balanceBeforeSell);
+            if (toSellLiquidity > lpInMyAddress) {
+                assert(poolId != -1);
+                uint256 withdrawAmt = toSellLiquidity - lpInMyAddress; 
+                lpAmtInPool -= withdrawAmt;
+                IMasterChef(masterChef).withdraw(uint256(poolId), withdrawAmt);
+            }
+
+            pair.approve(_sushiswap, toSellLiquidity);
+            (uint amountS, uint amountP) = IUniswapV2(_sushiswap).removeLiquidity(_supplyToken, _pairToken, toSellLiquidity, 
+                0, 0, address(this), block.timestamp + 1800);
+
+            IERC20(_pairToken).safeIncreaseAllowance(_sushiswap, amountP);
+            address[] memory paths = new address[](2);
+            paths[0] = _pairToken;
+            paths[1] = _supplyToken;
+            IUniswapV2(_sushiswap).swapExactTokensForTokens(
+                amountP,
+                (amtFromUniswap - amountS) * (1e18 - maxSlippage) / 1e18,
+                paths,
+                address(this),
+                block.timestamp + 1800
+            );
+
+            _sellAmount = IERC20(_supplyToken).balanceOf(address(this));
+        }
+
+        IERC20(_supplyToken).safeTransfer(msg.sender, _sellAmount);
+        return _sellAmount;
+    }
+
+    function min(uint256 x, uint256 y) internal pure returns (uint256 z) {
+        return x <= y ? x : y;
+    }
+
+    function adjust() external onlyController {
+        address _supplyToken = supplyToken;
+        address _pairToken = pairToken;
+        address _sushiswap = sushiswap;
+
+        uint256 balance = IERC20(_supplyToken).balanceOf(address(this));
+        require(balance > 0, "StrategyUniswapV2: no balance");
+
+        uint256 toBuy = min(balance, maxOneDeposit);
+        uint256 half = toBuy / 2;
+        IUniswapV2Pair pair = IUniswapV2Pair(SushiswapLibrary.pairFor(IUniswapV2(_sushiswap).factory(), _supplyToken, _pairToken));
+        
+        // swap half for pair token
+        {
+            IERC20(_supplyToken).safeIncreaseAllowance(_sushiswap, half);
+            address[] memory paths = new address[](2);
+            paths[0] = _supplyToken;
+            paths[1] = _pairToken;
+
+            (uint reserve0, uint reserve1,) = pair.getReserves();
+            uint256 expectOut = SushiswapLibrary.getAmountOut(half, reserve0, reserve1);
+
+            IUniswapV2(_sushiswap).swapExactTokensForTokens(
+                half,
+                expectOut * (1e18 - maxSlippage) / 1e18,
+                paths,
+                address(this),
+                block.timestamp + 1800
+            );
+        }
+
+        uint256 swappedPairTokenAmt = IERC20(_pairToken).balanceOf(address(this));
+        IERC20(_supplyToken).safeIncreaseAllowance(_sushiswap, half);
+        IERC20(_pairToken).safeIncreaseAllowance(_sushiswap, swappedPairTokenAmt);
+        IUniswapV2(_sushiswap).addLiquidity(_supplyToken, _pairToken, half, swappedPairTokenAmt, 0, 0, address(this), block.timestamp + 1800);
+
+        // deposit into pool for Sushi farming
+        if (poolId != -1) {
+            uint256 lpInMyAddress = pair.balanceOf(address(this));
+            pair.approve(masterChef, lpInMyAddress);
+            IMasterChef(masterChef).deposit(uint256(poolId), lpInMyAddress);
+            lpAmtInPool += lpInMyAddress;
+        }
+    }
+
+    function harvest() external override {
+        // TODO: consider to swap the left pair token in my address back to supply token
+
+        if (poolId != -1) {
+            IMasterChef(masterChef).withdraw(uint256(poolId), 0); // to trigger sushi harvest only
+            uint256 sushiBalance = IERC20(sushi).balanceOf(address(this));
+            if (sushiBalance > 0) {
+                // Sell Sushi token for obtain more supplying token(e.g. USDC)
+                IERC20(sushi).safeIncreaseAllowance(sushiswap, sushiBalance);
+
+                address[] memory paths = new address[](2);
+                paths[0] = sushi;
+                paths[1] = supplyToken;
+
+                IUniswapV2(sushiswap).swapExactTokensForTokens(
+                    sushiBalance,
+                    uint256(0),
+                    paths,
+                    address(this),
+                    block.timestamp + 1800
+                );
+            }
+        }
+    }
+
+    function setMaxOneDeposit(uint256 _maxOneDeposit) external onlyController {
+        maxOneDeposit = _maxOneDeposit;
+    }
+
+    function setMaxSlippage(uint256 _maxSlippage) external onlyController {
+        maxSlippage = _maxSlippage;
+    }
+}
---
+++ contracts/strategies/sushiswap/SushiswapLibrary.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.6;
+
+import "../interfaces/uniswap/IUniswapV2Pair.sol";
+
+library SushiswapLibrary {
+
+    // returns sorted token addresses, used to handle return values from pairs sorted in this order
+    function sortTokens(address tokenA, address tokenB) internal pure returns (address token0, address token1) {
+        require(tokenA != tokenB, 'UniswapV2Library: IDENTICAL_ADDRESSES');
+        (token0, token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
+        require(token0 != address(0), 'UniswapV2Library: ZERO_ADDRESS');
+    }
+
+    // calculates the CREATE2 address for a pair without making any external calls
+    function pairFor(address factory, address tokenA, address tokenB) internal pure returns (address pair) {
+        (address token0, address token1) = sortTokens(tokenA, tokenB);
+        pair = address(uint160(uint(keccak256(abi.encodePacked(
+                hex'ff',
+                factory,
+                keccak256(abi.encodePacked(token0, token1)),
+                hex'e18a34eb0e04b04f7a0ac29a6e80748dca96319b42c54d679cb821dca90c6303' // init code hash
+            )))));
+    }
+
+    // fetches and sorts the reserves for a pair
+    function getReserves(address factory, address tokenA, address tokenB) internal view returns (uint reserveA, uint reserveB) {
+        (address token0,) = sortTokens(tokenA, tokenB);
+        (uint reserve0, uint reserve1,) = IUniswapV2Pair(pairFor(factory, tokenA, tokenB)).getReserves();
+        (reserveA, reserveB) = tokenA == token0 ? (reserve0, reserve1) : (reserve1, reserve0);
+    }
+
+    // given an input amount of an asset and pair reserves, returns the maximum output amount of the other asset
+    function getAmountOut(uint amountIn, uint reserveIn, uint reserveOut) internal pure returns (uint amountOut) {
+        require(amountIn > 0, 'UniswapV2Library: INSUFFICIENT_INPUT_AMOUNT');
+        require(reserveIn > 0 && reserveOut > 0, 'UniswapV2Library: INSUFFICIENT_LIQUIDITY');
+        uint amountInWithFee = amountIn * 997;
+        uint numerator = amountInWithFee * reserveOut;
+        uint denominator = reserveIn * 1000 + amountInWithFee;
+        amountOut = numerator / denominator;
+    }
+}
