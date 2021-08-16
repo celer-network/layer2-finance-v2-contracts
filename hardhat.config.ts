dbd90c330ca9162c7b5238df694c9d9fccc33e90
Michael Zhou <zhoumotongxue008@gmail.com>
2021-08-16 17:53:31 +0800

    Update dependencies

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,10 +1,11 @@
 import '@nomiclabs/hardhat-ethers';
 import '@nomiclabs/hardhat-waffle';
 import '@typechain/hardhat';
-import * as dotenv from 'dotenv';
 import 'hardhat-contract-sizer';
 import 'hardhat-deploy';
 import 'hardhat-gas-reporter';
+
+import * as dotenv from 'dotenv';
 import { HardhatUserConfig } from 'hardhat/types';
 
 dotenv.config();
